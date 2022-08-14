b5327af468ce88ac03fff7cf339593d13a71f682
FJ <yongfahjin@gmail.com>
2022-08-14 17:20:40 +0800

    finished featured posts

---
+++ components/layout/Logo.js
@@ -0,0 +1,6 @@
+import classes from '../layout/Logo.module.css';
+const Logo = () => {
+  return <div className={classes.logo}>FJ next blog</div>;
+};
+
+export default Logo;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,36 @@
+import FeaturedPosts from '../components/home-page/FeaturedPosts';
 import Hero from '../components/home-page/Hero';
 
 const HomePage = () => {
+  const DUMMY_POSTS = [
+    {
+      slug: 'getting-started-with-nextjs',
+      title: 'Getting Started With NextJS',
+      image: 'getting-started-nextjs.png',
+      excerpt: 'NextJS is the React Framework for production',
+      date: '2022-10-10',
+    },
+    {
+      slug: 'getting-started-with-nextj2s',
+      title: 'Getting Started With NextJS',
+      image: 'getting-started-nextjs.png',
+      excerpt: 'NextJS is the React Framework for production',
+      date: '2022-10-10',
+    },
+    {
+      slug: 'getting-started-with-nextjs3',
+      title: 'Getting Started With NextJS',
+      image: 'getting-started-nextjs.png',
+      excerpt: 'NextJS is the React Framework for production',
+      date: '2022-10-10',
+    },
+  ];
+
   return (
-    <div>
+    <>
       <Hero />
-    </div>
+      <FeaturedPosts posts={DUMMY_POSTS} />
+    </>
   );
 };
 
