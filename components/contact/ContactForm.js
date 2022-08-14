df28c82010172d7d9c122a0dcdfd383af246a888
FJ <yongfahjin@gmail.com>
2022-08-14 21:55:53 +0800

    created contact form, prior to mongodb connection

---
+++ components/contact/ContactForm.js
@@ -0,0 +1,65 @@
+import { useState } from 'react';
+import classes from './ContactForm.module.css';
+const ContactForm = () => {
+  const [enteredEmail, setEnteredEmail] = useState('');
+  const [enteredName, setEnteredName] = useState('');
+  const [enteredMessage, setEnteredMessage] = useState('');
+  const sendMessageHandler = (e) => {
+    e.preventDefault();
+    fetch('/api/contact', {
+      method: 'POST',
+      body: JSON.stringify({
+        email: enteredEmail,
+        name: enteredName,
+        message: enteredMessage,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  };
+  return (
+    <section className={classes.contact}>
+      <h1>How can I help you? </h1>
+      <form className={classes.form} onSubmit={sendMessageHandler}>
+        <div className={classes.controls}>
+          <div className={classes.control}>
+            <label htmlFor='email'>Email</label>
+            <input
+              type='email'
+              id='Email'
+              required
+              value={enteredEmail}
+              onChange={(e) => setEnteredEmail(e.target.value)}
+            />
+          </div>
+          <div className={classes.control}>
+            <label htmlFor='name'>Your Name</label>
+            <input
+              type='text'
+              id='name'
+              required
+              value={enteredName}
+              onChange={(e) => setEnteredName(e.target.value)}
+            />
+          </div>
+        </div>
+        <div className={classes.control}>
+          <label htmlFor='message'>Your Message</label>
+          <textarea
+            id='message'
+            rows='5'
+            required
+            value={enteredMessage}
+            onChange={(e) => setEnteredMessage(e.target.value)}
+          ></textarea>
+        </div>
+        <div className={classes.actions}>
+          <button>Send Message</button>
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default ContactForm;
