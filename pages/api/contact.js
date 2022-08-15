55606c499cf833fcf719f77802831dc9220feff4
FJ <yongfahjin@gmail.com>
2022-08-15 09:35:34 +0800

    got post request contact form working

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,4 +1,6 @@
-const handler = (req, res) => {
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const handler = async (req, res) => {
   if (req.method === 'POST') {
     const { name, email, message } = req.body;
 
@@ -18,7 +20,29 @@ const handler = (req, res) => {
       name,
       message,
     };
-    console.log(newMessage);
+
+    const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3q3c1.mongodb.net/my-site?retryWrites=true&w=majority`;
+    const client = new MongoClient(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverApi: ServerApiVersion.v1,
+    });
+    try {
+      await client.connect();
+    } catch (error) {
+      res.status(500).json({ message: 'could not connect to database' });
+      return;
+    }
+    const db = client.db();
+    try {
+      const result = await db.collection('messages').insertOne(newMessage);
+      newMessage.id = result.insertedId;
+    } catch (error) {
+      client.close();
+      res.status(500).json({ message: 'Storing message failed' });
+      return;
+    }
+    client.close();
     res
       .status(201)
       .json({ message: 'Successfully stored message', message: newMessage });
